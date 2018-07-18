99fad3328851d3a82b00c1f9693b4bff2e5d797d
ciyingzuo <huntercyz@outlook.com>
2018-07-18 23:56:43 +0800

    push

--- src/containers/CourseRow.js
+++ src/containers/CourseRow.js
@@ -1,11 +1,19 @@
-import React from 'react';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import {BrowserRouter as Router, Route, Link} from 'react-router-dom'
 
 class CourseRow extends React.Component {
     render() {
         return (
             <tr>
                 <td>
-                    {this.props.course.title}
+                    <Router>
+                        <div className="container-fluid">
+                            <Link to={'/'+this.props.course.title}>{this.props.course.title}</Link>
+                            <Route path={'/'+this.props.course.title}/>
+                        </div>
+
+                    </Router>
                 </td>
                 <td>
                     <button className="btn btn-danger"
